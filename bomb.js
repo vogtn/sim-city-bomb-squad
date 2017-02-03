90ce45f07eb079c585016f52a7b598e065394ab0
vogtn <nicolasjvogt@gmail.com>
2017-02-03 11:33:52 -0800

    adding final logic

--- bomb.js
+++ bomb.js
@@ -1,6 +1,64 @@
-console.log("javascript running");
+var time= 30; //My display in seconds
+var interval;
+var wireColors = ["blue-wire", "green-wire", "white-wire", "red-wire", "yellow-wire"];
+var successWire = null;
 
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM loaded");
 });
 
+function tick(){
+  time--;
+  document.getElementById('timer').innerHTML = time;
+  if(time < 10){
+    document.getElementById('timer').style.color = "red";
+  }
+  if(time <= 0){
+    clearInterval(interval);
+    gameOver();
+  }
+}
+
+function gameOver(){
+  //explode background
+  //remove the unexploded class from the body
+  //add an exploded class to the body
+  document.getElementsByTagName("body")[0].classList.remove("unexploded");
+  document.getElementsByTagName("body")[0].classList.add("exploded");
+}
+
+function reset(){
+  clearInterval(interval);
+  time = 31;
+  interval = setInterval(tick, 1000);
+
+  function getGoodWire(){
+    return getRandomInt(0,4);
+  }
+
+  successWire = wireColors[getGoodWire()];
+
+  //reset images
+  document.getElementsByTagName("body")[0].classList.remove("exploded");
+  document.getElementsByTagName("body")[0].classList.add("unexploded");
+  document.getElementById('timer').style.color = "green";
+  for(var i =0; i< wireColors.length; i++){
+    document.getElementById(wireColors[i]).src = "./img/uncut-" + wireColors[i] + ".png";
+  }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function cutWire(param){
+  param.src = "./img/cut-"+param.id+".png";
+  if(param.id == successWire){
+    alert('win!');
+    reset();
+  }else{
+    console.log('wrong!');
+  }
+}
